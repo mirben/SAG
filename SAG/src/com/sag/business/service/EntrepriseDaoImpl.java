3613456582309d8e0eed57d282fd1769de3fa398
tuan <nguyentuan132@gmail.com>
2014-03-11 16:55:48 +0100

    entreprise impl test

---
+++ SAG/src/com/sag/business/service/EntrepriseDaoImpl.java
@@ -0,0 +1,45 @@
+package com.sag.business.service;
+
+import java.util.Collection;
+
+import com.sag.business.model.Entreprise;
+
+public class EntrepriseDaoImpl implements EntrepriseDao{
+
+	@Override
+	public Entreprise chercherParID(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entreprise chercherParEmail(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Entreprise> chercherTous() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Entreprise> chercherTous(int offset, int limite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Entreprise sauvagarder(Entreprise entreprise) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean supprimer(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ SAG/src/com/sag/business/test/EntrepriseDaoImplTest.java
@@ -0,0 +1,39 @@
+package com.sag.business.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntrepriseDaoImplTest {
+
+	@Test
+	public void testChercherParID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChercherParEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChercherTous() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChercherTousIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSauvagarder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSupprimer() {
+		fail("Not yet implemented");
+	}
+
+}
