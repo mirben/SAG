554ffc669074911540fcb9154189c3489efb90ba
tuan <nguyentuan132@gmail.com>
2014-03-11 16:47:34 +0100

    impl + test Domaine

---
+++ SAG/src/com/sag/business/service/DomaineDaoImpl.java
@@ -0,0 +1,60 @@
+package com.sag.business.service;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sag.business.model.Domaine;
+
+@Repository("domaineDao")
+@Transactional(readOnly = true)
+public class DomaineDaoImpl implements DomaineDao {
+	private EntityManager em;
+
+	@PersistenceContext
+	public void setEntityManager(EntityManager em) {
+		this.em = em;
+	}
+
+	@Override
+	public Domaine chercherParID(int id) {
+		return em.find(Domaine.class, id);
+
+	}
+
+	@Override
+	public Domaine chercherParNom(String nom) {
+		return (Domaine) em
+				.createQuery("select d from Domaine d where d.nom = nom")
+				.setParameter("nom", nom).getSingleResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<Domaine> chercherTous() {
+		return em.createQuery("FROM Domaine", Domaine.class).getResultList();
+	}
+
+	@Override
+	public Collection<Domaine> chercherTous(int offset, int limite) {
+		return em.createQuery("FROM Domaine", Domaine.class)
+				.setFirstResult(offset).setMaxResults(limite).getResultList();
+
+	}
+
+	@Override
+	public Domaine sauvagarder(Domaine domaine) {
+		return em.merge(domaine);
+	}
+
+	@Override
+	public Boolean supprimer(int id) {
+		em.remove(id);
+		return (chercherParID(id) == null);
+	}
+
+}
---
+++ SAG/src/com/sag/business/test/DomaineDaoImplTest.java
@@ -0,0 +1,51 @@
+package com.sag.business.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sag.business.model.Domaine;
+import com.sag.business.service.DomaineDaoImpl;
+
+public class DomaineDaoImplTest {
+	DomaineDaoImpl domaineDao;
+	
+
+	@Test
+	public void testChercherParID() {
+		System.out.println(domaineDao.chercherParID(1));
+		assertNotNull(domaineDao.chercherParID(1));
+		
+	}
+
+	@Test
+	public void testChercherTous() {
+		System.out.println(domaineDao.chercherTous());
+
+		assertNotEquals(1, domaineDao.chercherTous().size());
+	}
+
+	@Test
+	public void testChercherTousIntInt() {
+		System.out.println(domaineDao.chercherTous(1,5));
+		int actual = domaineDao.chercherTous(1,5).size();
+		int expected = 5;
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testSauvagarder() {
+		Domaine domaine = new Domaine();
+		domaine.setNom("Informatique");
+		domaineDao.sauvagarder(domaine);
+		
+		
+	}
+
+	@Test
+	public void testSupprimer() {
+		fail("Not yet implemented");
+	}
+
+}
