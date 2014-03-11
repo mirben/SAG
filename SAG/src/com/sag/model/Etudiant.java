64917a4dc496cb85fe85667b56b3e1b2a6babbd8
benjamin <benjamin.miretti@gmail.com>
2014-03-11 11:03:33 +0100

    Etudiant et entreprise ajoutés

---
+++ SAG/src/com/sag/model/Etudiant.java
@@ -0,0 +1,67 @@
+package com.sag.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@PrimaryKeyJoinColumn(name="ID_U")
+public class Etudiant {
+	private String logENT;
+	private String nom; 
+	private String prenom;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dateNaiss;
+	private String adresse;
+	private String siteWeb;
+	private String formation;
+	
+	public String getLogENT() {
+		return logENT;
+	}
+	public void setLogENT(String logENT) {
+		this.logENT = logENT;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaiss() {
+		return dateNaiss;
+	}
+	public void setDateNaiss(Date dateNaiss) {
+		this.dateNaiss = dateNaiss;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	public String getSiteWeb() {
+		return siteWeb;
+	}
+	public void setSiteWeb(String siteWeb) {
+		this.siteWeb = siteWeb;
+	}
+	public String getFormation() {
+		return formation;
+	}
+	public void setFormation(String formation) {
+		this.formation = formation;
+	}
+	
+
+}
--- SAG/src/com/sag/model/Utilisateur.java
+++ SAG/src/com/sag/model/Utilisateur.java
@@ -1,5 +1,42 @@
 package com.sag.model;
 
-public class Utilisateur {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@Entity
+@Inheritance(strategy=InheritanceType.JOINED)
+public class Utilisateur {
+	@Id()
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	private String statut;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getStatut() {
+		return statut;
+	}
+	public void setStatut(String statut) {
+		this.statut = statut;
+	}
+	
+	
 }
