4daba17f1a460ea3e83d629068f39bcd157ff97b
n306279 <n306279@054-023-00A1-03.luminy.univ-amu.fr>
2014-03-12 14:50:15 +0100

    ajoutet OffreDaoImpl

---
+++ SAG/src/com/sag/business/service/OffreDaoImlp.java
@@ -0,0 +1,47 @@
+package com.sag.business.service;
+
+import java.util.Collection;
+
+import com.sag.business.model.Offre;
+
+
+/**
+ * @version 1
+ * @author tuan 
+ * classe implémentation de OfferDao
+ */
+
+
+public class OffreDaoImlp implements OffreDao {
+
+	@Override
+	public Offre chercherParID(int id) {
+		
+		return null;
+	}
+
+	@Override
+	public Collection<Offre> chercherTous() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Offre> chercherTous(int offset, int limite) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Offre sauvagarder(Offre offre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean supprimer(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
