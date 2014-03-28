58bec43d2eb42a757053dea1206eae779726e27c
mirben <benjamin.miretti@gmail.com>
2014-03-28 22:55:00 +0100

    Update EntrepriseDaoTest.java

--- SAG/src/com/sag/business/test/EntrepriseDaoTest.java
+++ SAG/src/com/sag/business/test/EntrepriseDaoTest.java
@@ -17,19 +17,31 @@ import com.sag.business.model.StatutUtilisateur;
 import com.sag.business.service.EntrepriseDao;
 
 /**
- * Classe de test des méthode de EntrepriseDao
+ * Classe de test des méthode de EntrepriseDaoImpl
  * 
- * @version 1
- * @author tuan
+ * @author Tuan NGUYEN
  * 
  */
-
 public class EntrepriseDaoTest {
 
+       /**
+	*Le contexte de l'application
+	*/
 	private static Context initial;
+	
+       /**
+	* La Dao des entreprises
+	*/
 	private static EntrepriseDao entrepriseDao;
+	
+	/**
+	 * Liste des entreprises de test
+	 */
 	public static Vector<Entreprise> entreprisesTest;
 
+	/**
+	 * Méthode Afterclass qui nettoie les données de test inséré dans la base
+	 */
 	@AfterClass
 	public static void clean() {
 		for (Entreprise curEtpr : entreprisesTest) {
@@ -38,10 +50,9 @@ public class EntrepriseDaoTest {
 	}
 
 	/**
-	 * 
+	 * Méthode Beforeclass qui récupère la Dao et initialise les données de test
 	 * @throws NamingException
 	 */
-
 	@BeforeClass
 	public static void init() throws NamingException {
 		initial = new InitialContext();
@@ -67,7 +78,10 @@ public class EntrepriseDaoTest {
 		entreprisesTest.add(entrepriseDao.sauvegarder(testEtpr2));
 
 	}
-
+	
+	/**
+	 * Méthode de test pour sauvegarder()
+	 */
 	@Test
 	public void testSauvegarder() {
 		System.out.println("**** Test de la méthode sauvagarder ****");
@@ -95,6 +109,9 @@ public class EntrepriseDaoTest {
 
 	}
 
+	/**
+	 * Méthode de test pour chercherParID()
+	 */
 	@Test
 	public void testChercherParID() {
 		System.out.println("**** Test de la méthode chercherParID ****");
@@ -113,6 +130,9 @@ public class EntrepriseDaoTest {
 
 	}
 
+	/**
+	 * Méthode de test pour chercherParEmail()
+	 */
 	@Test
 	public void testChercherParEmail() {
 		System.out.println("**** Test de la méthode chercherParEmail ****");
@@ -130,6 +150,9 @@ public class EntrepriseDaoTest {
 		assertNull(entrepriseDao.chercherParEmail("nop@nop.com"));
 	}
 
+	/**
+	 * Méthode de test pour chercherTous()
+	 */
 	@Test
 	public void testChercherTous() {
 		System.out.println("**** Test de la méthode chercherTous ****");
@@ -138,6 +161,9 @@ public class EntrepriseDaoTest {
 				.size());
 	}
 
+	/**
+	 * Méthode de test pour chercherTous(int, int)
+	 */
 	@Test
 	public void testChercherTousIntInt() {
 		System.out.println("**** Test de la méthode chercherTous [a, b] ****");
@@ -150,6 +176,9 @@ public class EntrepriseDaoTest {
 
 	}
 
+	/**
+	 * Méthode de test pour supprimer()
+	 */
 	@Test
 	public void testSupprimer() {
 		System.out.println("**** Test de la méthode Supprimer  ****");
@@ -170,6 +199,9 @@ public class EntrepriseDaoTest {
 		assertFalse(entrepriseDao.supprimer(0));
 	}
 
+	/**
+	 * Méthode de test pour chercherRoleParID()
+	 */
 	public void testChercherRoleParID() {
 		
 		System.out.println("**** Test de la méthode ChercherRoleParID ****");
